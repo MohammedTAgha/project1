a94848f5a37c8fca9a2e331c08cee86274953cd2
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-06-27 18:39:07 +0300

    button

---
+++ src/components/common/navBar/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ts from '../../../styles/typography.module.css';
+import st from './Button.module.css'; // Assuming you have a CSS module file for the button component
+function Button({ text, onClick }) {
+  return (
+    <button className={`${st.button} ${ts.title}`} onClick={onClick}>
+      {text}
+    </button>
+  );
+}
+
+export default Button;
