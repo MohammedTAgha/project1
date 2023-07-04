cb59ac99a03f8effca2313b89b63a0ac28fa64e5
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-07-04 15:02:06 +0300

    create container components

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  width: ${(props) => props.width || '100%'};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default class Container extends Component {
+  render() {
+    const { width, children } = this.props;
+    return (
+      <StyledContainer width={width}>
+        {children}
+      </StyledContainer>
+    );
+  }
+}
