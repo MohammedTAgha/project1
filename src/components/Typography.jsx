e6ea325ac271921c7ca47e2a98dfc9448cc5e3cf
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-07-08 00:51:22 +0300

    dynamic typography

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -3,15 +3,15 @@ import styled from 'styled-components';
  const Hero = styled.h1`
   font-size: 2.8125rem;
   font-family: Roboto;
-  font-weight: 700;
-  color: ${props => (props.secondary ? '#FFCE6D' : '#FFFCCC'  )};
+  font-weight: ${props => props.fontWeight || 700};
+  color: ${props => props.secondary ? '#FFCE6D' : props.color || '#FFFCCC'};
 `;
 
  const MainHeading = styled.h1`
   font-size: 2.25rem;
   font-family: Dancing Script;
-  font-weight: 800;
-  color: ${props => (props.secondary ? '#FFCE6D' : '#FFFCCC'  )};
+  font-weight: ${props => props.fontWeight || 800};
+  color: ${props => props.secondary ? '#FFCE6D' : props.color || '#FFFCCC'};
   line-height: 117.3%;
   letter-spacing: 0.225rem;
 `;
@@ -19,60 +19,61 @@ import styled from 'styled-components';
  const Subheading = styled.h2`
   font-size: 1.875rem;
   font-family: Roboto;
-  font-weight: 700;
+  font-weight: ${props => props.fontWeight || 700};
   line-height: 117.3%;
-  color: ${props => (props.secondary ? '#FFCE6D' : '#FFFCCC'  )};
+  color: ${props => props.secondary ? '#FFCE6D' : props.color || '#FFFCCC'};
 `;
 
  const Title = styled.h3`
   font-size: 1.375rem;
   font-family: Roboto;
-  font-weight: 700;
-  color: ${props => (props.secondary ? '#FFCE6D' : '#FFFCCC'  )};
+  font-weight: ${props => props.fontWeight || 700};
+  color: ${props => props.secondary ? '#FFCE6D' : props.color || '#FFFCCC'};
 `;
 
  const Subtitle = styled.h4`
   font-size: 1.125rem;
   font-family: Roboto;
-  font-weight: 700;
-  color: ${props => (props.secondary ? '#FFCE6D' : '#FFFCCC'  )};
+  font-weight: ${props => props.fontWeight || 700};
+  color: ${props => props.secondary ? '#FFCE6D' : props.color || '#FFFCCC'};
 `;
 
  const BodyText = styled.p`
   font-size: 1rem;
   font-family: Roboto;
-  color: ${props => (props.secondary ? '#FFCE6D' : '#FFFCCC'  )};
+  color: ${props => props.secondary ? '#FFCE6D' : props.color || '#FFFCCC'};
 `;
 
  const XXL = styled.h1`
   font-size: 3rem;
   font-family: Roboto;
-  font-weight: 700;
+  font-weight: ${props => props.fontWeight || 700};
   line-height: 117.3%;
-  color: ${props => (props.primary ? '#FFFCCC' : '#FFCE6D')};
+  color: ${props => props.secondary ? '#FFCE6D' : props.color || '#FFFCCC'};
 `;
 
  const XL = styled.h2`
   font-size: 2rem;
   font-family: Roboto;
-  font-weight: 800;
+  font-weight: ${props => props.fontWeight || 800};
   letter-spacing: 0.04rem;
-  color: ${props => (props.primary ? '#FFFCCC' : '#FFCE6D')};
+  color: ${props => props.secondary ? '#FFCE6D' : props.color || '#FFFCCC'};
 `;
 
  const L = styled.h3`
   font-size: 1.5rem;
   font-family: Roboto;
   font-style: normal;
-  font-weight: 700;
+  font-weight: ${props => props.fontWeight || 700};
   line-height: 117.3%;
-  color: ${props => (props.primary ? '#FFFCCC' : '#FFCE6D')};
+   color: ${props => props.secondary ? '#FFCE6D' : props.color || '#FFFCCC'};
 `;
 
  const SmallText = styled.span`
   font-size: 0.875rem;
   font-family: Roboto;
-  color: ${props => (props.primary ? '#FFFCCC' : '#FFCE6D')};
+  color: ${props => props.secondary ? '#FFCE6D' : props.color || '#FFFCCC'};
+  font-weight: ${props => props.fontWeight};
 `;
 
 
